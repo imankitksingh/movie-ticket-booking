1236baac00408d042c0348f02d75417676b7b660
imankitksingh <ankitsingh980111@gmail.com>
2025-06-27 17:02:42 +0530

    Update DB config

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+    mongoose.connection.on("connected", () => console.log(" MongoDB Connected"));
+    mongoose.connection.on("disconnected", () => console.log(" MongoDB Disconnected"));
+
     try {
-        mongoose.connection.on("connected", () => console.log("Database Connected"));
-        await mongoose.connect(`${process.env.MONGODB_URI}/minecraftmovie`)
+        await mongoose.connect(`${process.env.MONGODB_URI}/minecraftmovie`);
     } catch (error) {
-        console.log(error.message)
-        mongoose.connection.on('disconnected', () => console.log('MongoDB disconnected'));
+        console.error("MongoDB connection error:", error.message);
     }
-}
+};
 
 export default connectDB;
